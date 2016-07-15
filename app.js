a4690ad45583c9c27e06aed6f7abb0be7391713c
sazha13 <sazha@bk.ru>
2016-07-15 20:43:49 +0400

    123

---
+++ app.js
@@ -0,0 +1,7 @@
+var http = require("http");
+console.log("hello world123");
+http.createServer(function(request, response) {
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.write("Hello World");
+  response.end();
+}).listen(8888);
