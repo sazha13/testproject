9cc64ea644ad49dedb13d1d72716df5a27f6e750
sazha13 <sazha@bk.ru>
2016-07-15 23:31:41 +0400

    123

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 var express = require('express'), // require the restify library.
-  server = express(); // create an HTTP server.
+  app = express(); // create an HTTP server.
 
 // add a route that listens on http://localhost:5000/hello/world
-server.get('/', function (req, res, cb) {
+app.get('/', function (req, res, cb) {
   res.send("Hello World!");
   return cb();
 });
 
-server.listen(process.env.PORT || 5000, function () { // bind server to port 5000.
+var server = app.listen(process.env.PORT || 5000, function () { // bind server to port 5000.
   var host = server.address().address;
   var port = server.address().port;
 
